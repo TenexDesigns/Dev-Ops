bd5c8f1d175e5894b1c94814544dbd966b01582c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-12 12:33:21 +0300

    Create 1_Resourcs.c

---
+++ Resources_to_learn/1_Resourcs.c
@@ -0,0 +1,50 @@
+Certainly! Here are some recommended resources, books, and online platforms to help you learn and improve your skills in DevOps:
+
+### Online Learning Platforms and Courses:
+
+1. **Udemy:** Offers a variety of DevOps courses, ranging from beginner to advanced levels.
+
+2. **Coursera:** Offers courses from universities and institutions on various DevOps topics.
+
+3. **edX:** Provides courses from universities and organizations, including those related to DevOps practices.
+
+4. **Pluralsight:** Offers a wide range of DevOps courses, including hands-on labs and assessments.
+
+5. **LinkedIn Learning (formerly Lynda):** Offers video courses on various DevOps concepts and tools.
+
+6. **Cloud Provider Documentation:**
+   - **AWS Documentation:** Comprehensive guides on various AWS services and DevOps practices.
+   - **Azure Docs:** Detailed documentation on Microsoft Azure services and DevOps practices.
+   - **Google Cloud Documentation:** Guides and tutorials on Google Cloud Platform services and DevOps.
+
+### DevOps Books:
+
+1. **"The Phoenix Project: A Novel About IT, DevOps, and Helping Your Business Win"** by Gene Kim, Kevin Behr, and George Spafford.
+2. **"DevOps Handbook: How to Create World-Class Agility, Reliability, & Security in Technology Organizations"** by Gene Kim, Jez Humble, Patrick Debois, and John Willis.
+3. **"Continuous Delivery: Reliable Software Releases through Build, Test, and Deployment Automation"** by Jez Humble and David Farley.
+4. **"Site Reliability Engineering: How Google Runs Production Systems"** by Niall Richard Murphy, Betsy Beyer, Chris Jones, and Jennifer Petoff.
+5. **"Docker Deep Dive"** by Nigel Poulton.
+
+### Websites and Blogs:
+
+1. **DevOps.com:** Provides news, articles, and resources related to DevOps practices.
+2. **Dev.to:** A community platform for developers with articles and discussions on DevOps topics.
+3. **The DevOps Institute:** Offers resources, webinars, and certification programs related to DevOps practices.
+4. **Medium:** Many DevOps experts and practitioners share insights and tutorials on Medium.
+
+### YouTube Channels and Videos:
+
+1. **The DevOps Lab:** A Microsoft Azure-focused channel with tutorials on DevOps practices and tools.
+2. **Traversy Media:** Offers tutorials on various DevOps tools and concepts.
+3. **Techworld with Nana:** Provides tutorials on Docker, Kubernetes, and other DevOps technologies.
+
+### Community and Forums:
+
+1. **Reddit:** Check out the DevOps subreddit for discussions, questions, and resources.
+2. **Stack Overflow:** A great place to ask specific questions and find answers related to DevOps.
+
+Remember that DevOps is a dynamic field, and the best way to learn is by combining theoretical knowledge with hands-on practice. Set up a lab environment, work on personal projects, and seek opportunities to apply what you learn. As you progress, consider obtaining relevant certifications to showcase your expertise to potential employers.
+
+
+
+  
